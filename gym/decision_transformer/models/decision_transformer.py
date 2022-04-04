182116cebb57637a1c76e8ba631e02bcc81045da
ben <benyamin.jami76@gmail.com>
2022-04-03 22:05:28 -0400

    stacked version

--- gym/decision_transformer/models/decision_transformer.py
+++ gym/decision_transformer/models/decision_transformer.py
@@ -28,7 +28,7 @@ class DecisionTransformer(TrajectoryModel):
         self.hidden_size = hidden_size
         config = transformers.GPT2Config(
             vocab_size=1,  # doesn't matter -- we don't use the vocab
-            n_embd=hidden_size,
+            n_embd=3*hidden_size,
             **kwargs
         )
 
@@ -36,12 +36,12 @@ class DecisionTransformer(TrajectoryModel):
         # is that the positional embeddings are removed (since we'll add those ourselves)
         self.transformer = GPT2Model(config)
 
-        self.embed_timestep = nn.Embedding(max_ep_len, hidden_size)
+        self.embed_timestep = nn.Embedding(max_ep_len, 3*hidden_size)
         self.embed_return = torch.nn.Linear(1, hidden_size)
         self.embed_state = torch.nn.Linear(self.state_dim, hidden_size)
         self.embed_action = torch.nn.Linear(self.act_dim, hidden_size)
 
-        self.embed_ln = nn.LayerNorm(hidden_size)
+        self.embed_ln = nn.LayerNorm(hidden_size*3)
 
         # note: we don't predict states or returns for the paper
         self.predict_state = torch.nn.Linear(hidden_size, self.state_dim)
@@ -65,26 +65,28 @@ class DecisionTransformer(TrajectoryModel):
         time_embeddings = self.embed_timestep(timesteps)
 
         # time embeddings are treated similar to positional embeddings
-        state_embeddings = state_embeddings + time_embeddings
-        action_embeddings = action_embeddings + time_embeddings
-        returns_embeddings = returns_embeddings + time_embeddings
+        # state_embeddings = state_embeddings + time_embeddings
+        # action_embeddings = action_embeddings + time_embeddings
+        # returns_embeddings = returns_embeddings + time_embeddings
 
         # this makes the sequence look like (R_1, s_1, a_1, R_2, s_2, a_2, ...)
         # which works nice in an autoregressive sense since states predict actions
         stacked_inputs = torch.stack(
             (returns_embeddings, state_embeddings, action_embeddings), dim=1
-        ).permute(0, 2, 1, 3).reshape(batch_size, 3 * seq_length, self.hidden_size)
+        ).permute(0, 2, 1, 3).reshape(batch_size, seq_length, 3 * self.hidden_size)
+        stacked_inputs = stacked_inputs + time_embeddings
+
         stacked_inputs = self.embed_ln(stacked_inputs)
 
         # to make the attention mask fit the stacked inputs, have to stack it as well
-        stacked_attention_mask = torch.stack(
-            (attention_mask, attention_mask, attention_mask), dim=1
-        ).permute(0, 2, 1).reshape(batch_size, 3 * seq_length)
+        # stacked_attention_mask = torch.stack(
+        #     (attention_mask, attention_mask, attention_mask), dim=1
+        # ).permute(0, 2, 1).reshape(batch_size, seq_length)
 
         # we feed in the input embeddings (not word indices as in NLP) to the model
         transformer_outputs = self.transformer(
             inputs_embeds=stacked_inputs,
-            attention_mask=stacked_attention_mask,
+            attention_mask=attention_mask,
         )
         x = transformer_outputs['last_hidden_state']
 
